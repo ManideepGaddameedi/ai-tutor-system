163172382bb17c3133aff63e1b0d4b8c0372c0be
manideepgaddameedi <manideepgaddameedi1@gmail.com>
2026-03-14 05:23:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,102 +1,42 @@
-function registerUser(){
+async function askQuestion(){
 
-let name=document.getElementById("name").value
-let email=document.getElementById("email").value
-let password=document.getElementById("password").value
+let question=document.getElementById("question").value
 
-localStorage.setItem("name",name)
-localStorage.setItem("email",email)
-localStorage.setItem("password",password)
-
-alert("Registration successful")
-
-window.location="login.html"
-
-}
-
-function loginUser(){
-
-let email=document.getElementById("loginEmail").value
-let password=document.getElementById("loginPassword").value
-
-let savedEmail=localStorage.getItem("email")
-let savedPassword=localStorage.getItem("password")
-
-if(email===savedEmail && password===savedPassword){
-
-window.location="dashboard.html"
-
-}else{
-
-alert("Invalid login")
-
-}
-
-}
-
-window.onload=function(){
-
-let name=localStorage.getItem("name")
-
-let welcome=document.getElementById("welcomeText")
-
-if(welcome && name){
-
-welcome.innerText="Welcome "+name
-
-}
-
-}
-
-function logout(){
-
-localStorage.clear()
-
-window.location="login.html"
-
-}
-
-function askQuestion(){
-
-let question=document.getElementById("question").value.toLowerCase()
+if(question==="") return
 
 let chatbox=document.getElementById("chatbox")
 
 chatbox.innerHTML+="<p><b>Student:</b> "+question+"</p>"
 
-let answer=""
+const API_KEY="AIzaSyCAmOssbPvoxnr_VodqVJpSdMsMv3mLWmA"
 
-if(question.includes("array")){
-answer="Arrays store elements in contiguous memory locations."
+const response=await fetch(
+"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key="+API_KEY,
+{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+contents:[
+{
+parts:[
+{ text: question }
+]
 }
-else if(question.includes("binary search")){
-answer="Binary search divides the search range in half each time."
+]
+})
 }
-else if(question.includes("java")){
-answer="Java is an object oriented programming language."
-}
-else{
-answer="I am still learning this topic."
-}
+)
+
+const data=await response.json()
+
+let answer=data.candidates[0].content.parts[0].text
 
 chatbox.innerHTML+="<p><b>AI Tutor:</b> "+answer+"</p>"
 
 document.getElementById("question").value=""
 
-}
-
-function checkAnswer(ans){
-
-if(ans==="b"){
-
-document.getElementById("result").innerText="Correct Answer"
-
-localStorage.setItem("quizScore","1")
-
-}else{
-
-document.getElementById("result").innerText="Wrong Answer"
-
-}
+chatbox.scrollTop=chatbox.scrollHeight
 
 }

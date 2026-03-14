5c01263646beb1ad6e1d949a33f0ffb274bf48fc
manideepgaddameedi <manideepgaddameedi1@gmail.com>
2026-03-14 08:19:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,76 @@
+function registerUser(){
+
+let name=document.getElementById("name").value
+let email=document.getElementById("email").value
+let password=document.getElementById("password").value
+
+localStorage.setItem("name",name)
+localStorage.setItem("email",email)
+localStorage.setItem("password",password)
+
+alert("Registered Successfully")
+
+window.location="login.html"
+
+}
+
+
+
+function loginUser(){
+
+let email=document.getElementById("loginEmail").value
+let password=document.getElementById("loginPassword").value
+
+let savedEmail=localStorage.getItem("email")
+let savedPassword=localStorage.getItem("password")
+
+if(email===savedEmail && password===savedPassword){
+
+window.location="dashboard.html"
+
+}else{
+
+alert("Invalid Login")
+
+}
+
+}
+
+
+
+window.onload=function(){
+
+let name=localStorage.getItem("name")
+
+let welcome=document.getElementById("welcomeText")
+
+if(welcome){
+
+welcome.innerText="Welcome "+name
+
+}
+
+}
+
+
+
+function logout(){
+
+localStorage.clear()
+
+window.location="login.html"
+
+}
+
+
+
 async function askQuestion(){
 
 let question=document.getElementById("question").value
 
-if(question==="") return
-
 let chatbox=document.getElementById("chatbox")
 
-chatbox.innerHTML+="<p><b>Student:</b> "+question+"</p>"
+chatbox.innerHTML+="<p><b>You:</b> "+question+"</p>"
 
 const API_KEY="AIzaSyCAmOssbPvoxnr_VodqVJpSdMsMv3mLWmA"
 
@@ -20,9 +84,7 @@ headers:{
 body:JSON.stringify({
 contents:[
 {
-parts:[
-{ text: question }
-]
+parts:[{text:question}]
 }
 ]
 })
@@ -33,10 +95,8 @@ const data=await response.json()
 
 let answer=data.candidates[0].content.parts[0].text
 
-chatbox.innerHTML+="<p><b>AI Tutor:</b> "+answer+"</p>"
+chatbox.innerHTML+="<p><b>AI:</b> "+answer+"</p>"
 
 document.getElementById("question").value=""
-
-chatbox.scrollTop=chatbox.scrollHeight
 
 }
